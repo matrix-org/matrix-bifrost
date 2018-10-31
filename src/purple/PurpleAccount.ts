85eff2bb3c8756d8821cd4aecd0d2de4c60e660e
Will Hunt <will@half-shot.uk>
2018-10-31 18:29:48 +0000

    Add account manipulation functions

--- src/purple/PurpleAccount.ts
+++ src/purple/PurpleAccount.ts
@@ -6,11 +6,37 @@ import { helper, plugins, accounts } from "node-purple";
 import { PurpleProtocol } from "./PurpleInstance";
 
 export class PurpleAccount {
-    constructor(private username: string,private protocol: PurpleProtocol) {
+    private acctData: any;
+    private handle?: External;
+    private enabled: boolean;
+    constructor(private username: string, private _protocol: PurpleProtocol) {
+        this.enabled = false;
+    }
+    get name(): string { return this.acctData.name };
 
+    get protocol() : PurpleProtocol { return this._protocol };
+
+    get isEnabled(): boolean { return this.enabled };
+
+    findAccount() {
+        const data = accounts.find(this.username, this._protocol.id);
+        if (!data) {
+            throw new Error("Account not found");
+        }
+        this.handle = data.handle;
+        this.enabled = accounts.get_enabled(this.handle);
+        this.acctData = data;
     }
 
     createNew() {
-        accounts.new(this.username, this.protocol.id)
+        accounts.new(this.username, this._protocol.id);
     }
+
+    setEnabled(enable: boolean) {
+        accounts.set_enabled(this.handle, enable);
+    }
+
+    // connect() {
+    //     accounts.connect(this.username, this.protocol.id);
+    // }
 }
