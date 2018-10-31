e40a9cda79aa9980dcfa4ee858565f2aaca58da7
Will Hunt <will@half-shot.uk>
2018-10-31 01:29:38 +0000

    Implement account creation in libpurple

--- src/purple/PurpleAccount.ts
+++ src/purple/PurpleAccount.ts
@@ -1,6 +1,16 @@
 /**
  * An interface for storing account data inside the userstore.
  */
-export interface PurpleAccount {
 
+import { helper, plugins, accounts } from "node-purple";
+import { PurpleProtocol } from "./PurpleInstance";
+
+export class PurpleAccount {
+    constructor(private username: string,private protocol: PurpleProtocol) {
+
+    }
+
+    createNew() {
+        accounts.new(this.username, this.protocol.id)
+    }
 }
