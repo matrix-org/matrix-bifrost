e88fd07826562ca9025ea8538efa9f0e6f7bb8e2
Will Hunt <will@half-shot.uk>
2018-10-31 18:30:41 +0000

    Spit events from purple into an emitter

--- src/purple/PurpleInstance.ts
+++ src/purple/PurpleInstance.ts
@@ -1,4 +1,6 @@
+import { EventEmitter } from "events";
 import { helper, plugins } from "node-purple";
+import { PurpleAccount } from "./PurpleAccount";
 const log = require("matrix-appservice-bridge").Logging.get("PurpleInstance");
 
 export class PurpleProtocol {
@@ -14,16 +16,20 @@ export class PurpleProtocol {
     }
 }
 
-export class PurpleInstance {
+export class PurpleInstance extends EventEmitter {
     private protocols: PurpleProtocol[];
+    private accounts: Map<string, PurpleAccount>;
     constructor() {
+        super();
         this.protocols = [];
+        this.accounts = new Map();
     }
 
     public async start() {
         log.info("Starting purple instance");
         helper.setupPurple({
             debugEnabled: 1,
+            eventFunc: this.eventFunc.bind(this),
         });
         log.info("Started purple instance");
         this.protocols = plugins.get_protocols().map(
@@ -31,7 +37,31 @@ export class PurpleInstance {
         );
     }
 
+    public getAccount(username: string, protocolId: string): PurpleAccount {
+        const key = `${protocolId}://${username}`;
+        let acct = this.accounts.get(key);
+        if (!acct) {
+            const protocol = this.getProtocol(protocolId);
+            if (protocol === undefined) {
+                throw new Error("Protocol not found");
+            }
+            acct = new PurpleAccount(username, protocol);
+            acct.findAccount();
+            this.accounts.set(key, acct);
+        }
+        return acct;
+    }
+
+    public getProtocol(id: string){
+        return this.protocols.find((proto) => proto.id === id);
+    }
+
     public getProtocols(): PurpleProtocol[] {
         return this.protocols;
+    }
+
+    public eventFunc(eventName: string, data: any) {
+        log.verbose(`Got ${eventName} from purple`);
+        this.emit(eventName, data);
     }
 }
