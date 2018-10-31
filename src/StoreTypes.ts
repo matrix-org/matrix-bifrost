337438b5d94d9cec0b32b1dd455c377436358482
Will Hunt <will@half-shot.uk>
2018-10-31 18:27:59 +0000

    Add IM room type

--- src/StoreTypes.ts
+++ src/StoreTypes.ts
@@ -1,5 +1,15 @@
 export const MROOM_TYPE_UADMIN = "user-admin";
+export const MROOM_TYPE_IM = "im";
 
 export interface IMatrixRoomData {
-    type: string; // One of [MROOM_TYPE_UADMIN]
+    type: string; // One of [MROOM_TYPE_UADMIN, MROOM_TYPE_IM]
+}
+
+export interface IMatrixUserData {
+    accounts: {[key: string]: IMatrixUserAccount}
+}
+
+export interface IMatrixUserAccount {
+    username: string;
+    protocolId: string;
 }
