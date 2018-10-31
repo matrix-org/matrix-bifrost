9eda472fa8c8ea65ac0b7015219891195aad4d30
Will Hunt <will@half-shot.uk>
2018-10-31 01:28:49 +0000

    Add basic matrix types

---
+++ src/MatrixTypes.ts
@@ -0,0 +1,46 @@
+/**
+ * This should really live in matrix-appservice-bridge but that would be a lot of faff
+ * and timing is of the essence.
+ * - Half-Shot
+ */
+import { MatrixUser, RemoteUser, MatrixRoom, RemoteRoom } from "matrix-appservice-bridge";
+
+export interface IEventRequest {
+    getData(): IEventRequestData;
+    getDuration(): number;
+    getId(): string;
+    getPromise(): Promise<void>;
+}
+
+ /**
+  * This is actually just a matrix event, as far as we care.
+  */
+export interface IEventRequestData {
+    event_id: string;
+    origin_server_ts: number;
+    sender: string;
+    type: string;
+    content: any;
+    unsigned: any;
+    room_id: string;
+    state_key: string;
+}
+
+export interface IBridgeContext {
+    senders: {
+        matrix: MatrixUser|null,
+        remote: RemoteUser|null,
+        remotes: RemoteUser[],
+    },
+    targets: {
+        matrix: MatrixUser|null,
+        remote: RemoteUser|null,
+        remotes: RemoteUser[],
+    },
+    rooms: {
+        matrix: MatrixRoom|null,
+        remote: RemoteRoom|null,
+        remotes: RemoteRoom[],
+    }
+}
+
