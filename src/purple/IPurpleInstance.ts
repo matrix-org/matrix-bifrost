e50b7e981a0ea2267f13d3c1833753d7591b7e3a
Will Hunt <will@half-shot.uk>
2018-11-09 19:10:45 +0000

    Fixup types

--- src/purple/IPurpleInstance.ts
+++ src/purple/IPurpleInstance.ts
@@ -3,8 +3,8 @@ import { PurpleAccount } from "./PurpleAccount";
 
 export interface IPurpleInstance {
     start(config: any): Promise<void>
-    getAccount(username: string, protocolId: string): PurpleAccount;
+    getAccount(username: string, protocolId: string): PurpleAccount|null;
     getProtocol(id: string);
     getProtocols(): PurpleProtocol[];
-    on(name: string, cb : () =>void);
+    on(name: string, cb : (ev: any) =>void);
 }
--- src/purple/PurpleEvents.ts
+++ src/purple/PurpleEvents.ts
@@ -2,9 +2,12 @@
  * A collection of interfaces that may be emitted by PurpleInterface
  */
 
+ import { Account } from "node-purple";
+
+
  //received-im-msg
  export interface IReceivedImMsg {
     sender: string;
     message: string;
-    account: any;
+    account: Account;
  }
