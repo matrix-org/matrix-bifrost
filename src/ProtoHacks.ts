17a747f9b74ecdab02a118b342675c51596fa1f1
Will Hunt <will@half-shot.uk>
2018-11-12 11:05:52 +0000

    Add class for hacking around protocol specific issues

---
+++ src/ProtoHacks.ts
@@ -0,0 +1,18 @@
+import { IChatInvite } from "./purple/PurpleEvents";
+
+const PRPL_MATRIX = "prpl-matrix";
+
+/**
+ * This class hacks around issues with  certain protocols, it's not pretty
+ * but it allows us to work around issues with certain protocols.
+ */
+export class ProtoHacks {
+    static getRoomNameForInvite(invite: IChatInvite): string {
+        // prpl-matrix sends us an invite with the room name set to the
+        // matrix user's displayname, but the real room name is the room_id.
+        if (invite.account.protocol_id === PRPL_MATRIX) {
+            return invite.join_properties.room_id;
+        }
+        return invite.room_name;
+    }
+}
