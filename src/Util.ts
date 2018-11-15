97ccf02a717f380f5e54b5cf179535d931b9a07f
Will Hunt <will@half-shot.uk>
2018-11-15 03:35:42 +0000

    Don't strip out hosts for now

--- src/Util.ts
+++ src/Util.ts
@@ -1,5 +1,6 @@
 import { PurpleProtocol } from "./purple/PurpleInstance";
 import { MatrixUser } from "matrix-appservice-bridge";
+import { ProtoHacks } from "./ProtoHacks";
 
 export class Util {
 
@@ -13,12 +14,9 @@ export class Util {
         protocol: PurpleProtocol,
         senderId: string,
         domain: string,
-        prefix: string = ""): MatrixUser {
-        // XXX: XMPP senders have a /host appended to their sender.
-        // We're stripping them because they look ugly AF.
-        if (protocol.id === "prpl-jabber") {
-            senderId = senderId.split("/")[0];
-        }
+        prefix: string = "",
+        isGroupChat: boolean = false): MatrixUser {
+        senderId = ProtoHacks.getSenderId(protocol, senderId, isGroupChat);
         // This is a little bad, but we drop the prpl- because it's a bit ugly.
         const protocolName = protocol.id.startsWith("prpl-") ? protocol.id.substr("prpl-".length) : protocol.id;
         // senderId containing : can mess things up
