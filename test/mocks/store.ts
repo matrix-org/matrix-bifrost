51c731f4cc9229fe7ed7b8e09f32a386fd21c0f1
Will Hunt <will@half-shot.uk>
2019-01-30 19:36:29 +0000

    Add store mock

---
+++ test/mocks/store.ts
@@ -0,0 +1,17 @@
+import { Store } from "../../src/Store";
+import { RoomBridgeStore, UserBridgeStore } from "matrix-appservice-bridge";
+import * as Datastore from "nedb";
+
+const DEF_REGEX = /@remote/;
+
+export function mockStore(remoteUserRegex = DEF_REGEX) {
+    const userStore = new UserBridgeStore(new Datastore());
+    const roomStore = new RoomBridgeStore(new Datastore());
+    return new Store({
+        getRoomStore: () => roomStore,
+        getUserStore: () => userStore,
+        getBot: () => ({
+            isRemoteUser: (u) => remoteUserRegex.exec(u) !== null,
+        }),
+    });
+}
--- test/test_roomsync.ts
+++ test/test_roomsync.ts
@@ -4,7 +4,8 @@ import { PurpleProtocol } from "../src/purple/PurpleProtocol";
 import { RoomSync } from "../src/RoomSync";
 import { Deduplicator } from "../src/Deduplicator";
 import { create } from "domain";
-import { IRoomEntry } from "../src/StoreTypes";
+import { IRoomEntry, MROOM_TYPE_GROUP, IRemoteGroupData, MUSER_TYPE_ACCOUNT } from "../src/StoreTypes";
+import { mockStore } from "./mocks/store";
 const expect = Chai.expect;
 
 const dummyProtocol = new PurpleProtocol({
@@ -32,7 +33,10 @@ function createBotAndIntent() {
     return {bot, intent};
 }
 
+let remoteJoins: any[];
+
 function createRoomSync(rooms: IRoomEntry[] = []) {
+    remoteJoins = [];
     // Create dummy objects, only implement needed stuff.
     const purple = {
         on: (ev: string, func: () => void) => {
@@ -41,55 +45,66 @@ function createRoomSync(rooms: IRoomEntry[] = []) {
         getProtocol: () => true,
     };
 
-    const store = {
-        getRoomsOfType: (type: string) => rooms,
-        getRemoteUsersFromMxId: async (userId: string) => {
-            if (userId === "@foo:bar") {
-                return [{
-                    get: (key: string) => {
-                        return {
-                            username: "foobar",
-                            protocolId: dummyProtocol.id,
-                        }[key];
-                    },
-                    getId: () => "foobar",
-                }];
-            }
-            return [];
-        },
+    const gateway = {
+        rejoinRemoteUser: (user, room) => remoteJoins.push({user, room}),
     };
 
-    return new RoomSync(purple as any, store as any, new Deduplicator(), {} as any);
+    const store = mockStore();
+
+    // {
+    //     get: (key: string) => {
+    //         return {
+    //             username: "foobar",
+    //             protocolId: dummyProtocol.id,
+    //         }[key];
+    //     },
+    //     getId: () => "foobar",
+    // }
+
+    return {
+        rs: new RoomSync(purple as any, store, new Deduplicator(), gateway as any),
+        store,
+    };
 }
 
 describe("RoomSync", () => {
     it("constructs", () => {
         const rs = createRoomSync();
     });
-    it("should sync one room", async () => {
-        const rs = createRoomSync([
-            {
-                matrix: {
-                    getId: () => "!abc:foobar",
-                },
-                remote: {
-                    get: (key: string) => {
-                        return {
-                            protocol_id: dummyProtocol.id,
-                            room_name: "abc",
-                        }[key];
-                    },
-                    getId: () => "foobar",
-                },
-            },
-        ]);
+    it("should sync one room for one user", async () => {
+        const {rs, store} = createRoomSync();
         const {bot, intent} = createBotAndIntent();
+        await store.storeRoom("!abc:foobar", MROOM_TYPE_GROUP, "foobar", {
+            type: MROOM_TYPE_GROUP,
+            protocol_id: dummyProtocol.id,
+            room_name: "abc",
+        } as IRemoteGroupData);
+        await store.storeUser("@foo:bar", dummyProtocol, "foobar", MUSER_TYPE_ACCOUNT);
         await rs.sync(bot, intent);
-        expect(rs.getMembershipForUser("foobar")).to.deep.equal([
+        expect(rs.getMembershipForUser("prpl-dummy://foobar")).to.deep.equal([
             {
                 membership: "join",
                 params: {},
                 room_name: "abc",
+            },
+        ]);
+    });
+    it("should only sync remote users for gateways", async () => {
+        const {rs, store} = createRoomSync();
+        const {bot, intent} = createBotAndIntent();
+        await store.storeRoom("!abc:foobar", MROOM_TYPE_GROUP, "foobar", {
+            type: MROOM_TYPE_GROUP,
+            protocol_id: dummyProtocol.id,
+            room_name: "abc",
+            gateway: true,
+        } as IRemoteGroupData);
+        await store.storeUser("@foo:bar", dummyProtocol, "foobar", MUSER_TYPE_ACCOUNT);
+        await rs.sync(bot, intent);
+        expect(rs.getMembershipForUser("prpl-dummy://foobar")).to.not.exist;
+        expect(remoteJoins).to.deep.equal([
+            {
+                user: "@remote_foo:bar",
+                room: "!abc:foobar",
             },
         ]);
     });
