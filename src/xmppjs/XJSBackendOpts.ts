a13bdc6d544488cd3539098dbccef25b9c8dd3a7
Will Hunt <willh@matrix.org>
2021-09-13 14:08:11 +0100

    Add JingleConfig

--- src/xmppjs/XJSBackendOpts.ts
+++ src/xmppjs/XJSBackendOpts.ts
@@ -1,7 +1,12 @@
+
+export interface JingleConfig {
+    autodownload: boolean;
+}
 export interface IXJSBackendOpts {
     service: string;
     domain: string;
     password: string;
     defaultResource: string;
     logRawStream: boolean;
+    jingle?: JingleConfig;
 }
