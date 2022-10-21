bdf29769e9bbd7fb0e794842e6ae8c1e6202c07b
Half-Shot <will@half-shot.uk>
2022-10-21 17:14:34 +0100

    Add test structure

---
+++ spec/e2e/basic.spec.ts
@@ -0,0 +1,7 @@
+import { it, expect, describe } from '@jest/globals';
+
+describe('Some tests will eventually appear here', () => {
+    it('1+1 = 2',() => {
+        expect(1+1).toEqual(2);
+    })
+});
