60a3042eca62257dc026b03cd3e26cf19112a92b
Half-Shot <will@half-shot.uk>
2024-01-12 09:16:38 +0000

    Fix test.

--- test/xmppjs/util.ts
+++ test/xmppjs/util.ts
@@ -1,8 +1,8 @@
-import * as parser from "fast-xml-parser";
+import { XMLValidator } from "fast-xml-parser";
 import { AssertionError } from "chai";
 
 export function assertXML(xml) {
-    const err = parser.validate(xml);
+    const err = XMLValidator.validate(xml);
     if (err !== true) {
         throw new AssertionError(err.err.code + ": " + err.err.msg);
     }
